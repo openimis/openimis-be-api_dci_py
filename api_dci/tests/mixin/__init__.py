3d13bbd98282b40ada122aa45c3cede4b8bf5309
maxime ngoe <maxime.ngoe@gmail.com>
2026-04-09 08:40:23 +0100

    fix: :lipstick: Fix Python Flake 8

--- api_dci/tests/mixin/__init__.py
+++ api_dci/tests/mixin/__init__.py
@@ -2,4 +2,3 @@ from api_dci.tests.mixin.genericTestMixin import GenericTestMixin  # noqa: F401
 from api_dci.tests.mixin.dciApiTestMixin import DCIApiTestMixin  # noqa: F401
 from api_dci.tests.mixin.personTestMixin import PersonTestMixin  # noqa: F401
 from api_dci.tests.mixin.logInMixin import LogInMixin  # noqa: F401
-
